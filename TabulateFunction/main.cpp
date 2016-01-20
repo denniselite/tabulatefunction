2c2ca94ff8457406d350fec11bc13166f9add924
Denis Podlesnykh <denniselite@live.com>
2016-01-20 08:39:16 +0300

    Initial Commit

---
+++ TabulateFunction/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TabulateFunction
+//
+//  Created by Denis Podlesnykh on 20.01.16.
+//  Copyright © 2016 Denis Podlesnykh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
